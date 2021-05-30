a277e910eb7a9f7a9cbd9fbeb5320b5727d230b4
Nate Sawant <natesawant@gmail.com>
2021-05-30 11:47:43 -0400

    Add Base Material Types

---
+++ core/src/main/java/particlesimulatorgdx/Material.java
@@ -0,0 +1,25 @@
+package particlesimulatorgdx;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class Material {
+    private static String matType;
+    private static double matDensity;
+    private static Color matColor;
+
+    public static void main() {
+
+    }
+
+    public String getType() {
+        return matType;
+    }
+
+    public double getDensity() {
+        return matDensity;
+    }
+
+    public Color getColor() {
+        return matColor;
+    }
+}
---
+++ core/src/main/java/particlesimulatorgdx/SimulatorCore.java
@@ -0,0 +1,9 @@
+package particlesimulatorgdx;
+
+public class SimulatorCore {
+    private static Material[][];// = new Material[720][480];
+
+    public static void main(String[] args) {
+        
+    }
+}
